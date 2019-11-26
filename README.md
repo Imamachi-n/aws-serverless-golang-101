d1d65644044b5c718e72ad256cefd5ce9b80d171
Naoto Imamachi <naoto.imamachi@gmail.com>
2019-11-26 22:46:25 +0900

    Created a sample code for GET method

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # aws-serverless-golang-101
+
+To deploy lambda functions to AWS
+```bash
+make deploy
+```
+
+To remove lambda functions from AWS
+```bash
+make remove
+```
---
+++ helloworld/getHelloWorld.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type TestResponse struct {
+	Name string `json:"name"`
+	Msg  string `json:"msg"`
+}
+
+// Handler function Using AWS Lambda Proxy Request
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	//Get the path parameter that was sent
+	name := request.PathParameters["name"]
+
+	//Get the query parameter that was sent
+	msg := request.QueryStringParameters["msg"]
+
+	test := TestResponse{
+		Name: name,
+		Msg:  msg,
+	}
+	jsonRes, _ := json.Marshal(test)
+
+	//Generate message that want to be sent as body
+	// message := fmt.Sprintf(" { \"Message\" : \"Hello %s %s \" } ", name, msg)
+
+	//Returning response with AWS Lambda Proxy Response
+	return events.APIGatewayProxyResponse{
+		Body:       string(jsonRes),
+		StatusCode: 200,
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
